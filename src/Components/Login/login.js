53a39db9df2417e524db2ddfe362f6e761c67ac2
sakship-bit <sakshi22042001@gmail.com>
2022-12-13 15:03:58 +0530

    login and signup added

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -1,18 +1,30 @@
+import { useRef } from "react"
 import React from 'react'
 
-function Login() {
+const Login=() =>{
+    const emailInputRef=useRef();
+    const passwordInputRef=useRef();
+
+    const submitHandler=(e)=>{
+        e.preventDefault();
+        const enteredEmail=emailInputRef.current.value;
+        const enteredPassword=passwordInputRef.current.value;
+
+        
+    }
+
     return (
-        <div className='container-fluid justify-content-center col-3 bg-white mt-5 p-5'>
+        <div className='container-fluid justify-content-center col-lg-3 col-sm-6 col-8 bg-white mt-5 p-5'>
             <h4 className='text-dark text-center'>Login</h4>
-            <form>
+            <form onSubmit={submitHandler}>
                 <div class="form-group col-12 mt-4">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <input ref={emailInputRef} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
                         
                 </div>
                 <div class="form-group mt-3">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1"/>
+                    <input ref={passwordInputRef} type="password" class="form-control" id="exampleInputPassword1"required/>
                 </div>
                 
                 <button type="submit" class="btn btn-dark w-100 my-5">Login</button>
--- src/Components/Login/signup.js
+++ src/Components/Login/signup.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import { useRef } from 'react';
 
-function Signup() {
+const Signup=()=> {
+  const namesignInputRef=useRef();
+
+  const emailsignInputRef=useRef();
+  const passwordsignInputRef=useRef();
+
+
+  const signUp=(e)=>{
+      e.preventDefault();
+      const enteredEmail=emailsignInputRef.current.value;
+      const enteredPassword=passwordsignInputRef.current.value;
+      const enteredname=namesignInputRef.current.value;
+
+      
+  }
+
   return (
-    <div>Signup</div>
+      <div className='container-fluid justify-content-center col-lg-3 col-sm-6 col-8 bg-white mt-5 p-5'>
+          <h4 className='text-dark text-center'>Signup</h4>
+          <form onSubmit={signUp}>
+              <div className="form-group col-12 mt-4">
+                  <label htmlFor="name">Name</label>
+                  <input ref={namesignInputRef} type="text" className="form-control" id="name" required/>
+                      
+              </div>
+              <div className="form-group col-12 mt-3">
+                  <label htmlFor="email">Email</label>
+                  <input ref={emailsignInputRef} type="email" className="form-control" id="email" aria-describedby="emailHelp" required/>
+                      
+              </div>
+              <div className="form-group col-12 mt-3">
+                  <label htmlFor="password">Password</label>
+                  <input ref={passwordsignInputRef} type="password" className="form-control" id="password"required/>
+              </div>
+              
+              <button type="submit" className="btn btn-dark w-100 my-5">Create new account</button>
+          </form>
+      </div>
   )
 }
 
